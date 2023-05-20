b424767aab294b88de600ccecd5977eed4c238c6
Elycor74 <133437175+Elycor74@users.noreply.github.com>
2023-05-20 17:39:31 +0200

    Update Terminal.bat

--- Batch projects/WindowsXP/Terminal.bat
+++ Batch projects/WindowsXP/Terminal.bat
@@ -12,6 +12,7 @@ echo      MAIN MENU
 echo  =================
 echo.
 :Menu
+echo.
 echo Welcome to the Main Terminal, what would you like to do?
 echo.
 set /p where=
@@ -31,6 +32,7 @@ if not %where% == exit goto :error
 cd C:\Windows\system32
 
 :Menu1
+echo.
 echo What would you like to do next?
 echo.
 set /p where=
@@ -49,30 +51,39 @@ if not %where% == CMD goto :error
 if not %where% == exit goto :error
 
 :info
+echo.
 echo NOTE: this is a version adapted for Windows XP and versions before Windows 10
 echo for programs adapted to windows 10 and 11 consult the website at this link:
 echo ...
 echo Main Terminal is a program developed for help people using Windows XP
 echo for reading all the functions type 'commands' in the text space
+echo.
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :Menu1
 
 :commands
 echo This is the list of all the functions of this program:
-echo     INFO: Read all the infos about PDF_Editor Terminal.
-echo COMMANDS: Read all the functions of this program.
-echo    START: Run a PDF_Editor program.
+echo.
+echo     info: Read all the infos about PDF_Editor Terminal.
+echo commands: Read all the functions of this program.
+echo    start: Run a PDF_Editor program.
 echo      USB: Search for a USB path in the system.
 echo      CMD: Open the CMD terrminal.
-echo     EXIT: Leave the program.
+echo     exit: Leave the program.
+echo.
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :Menu1
 
 :start
+echo.
 echo This is the list of all the programs you can run:
 echo.
 echo 1Google
@@ -95,8 +106,9 @@ if not %where% == WindowsXPAV2.0 goto :error1
 if not %where% == exit goto :error1
 
 :usb
+echo.
 echo Searching for an USB path...
-pause
+echo.
 if exist [...] goto usb1
 if exist [...] goto usb2
 if exist [...] goto usb3
@@ -104,28 +116,41 @@ if not exist [...] goto clean
 if not exist [...] goto clean
 if not exist [...] goto clean
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :Menu1
 
 :cmd
-echo Starting CMD terminal....
+echo.
+echo Starting CMD terminal...
+echo.
 pause
+echo.
 start CMD
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :Menu1
 
 :clean
+echo.
 echo No USB path has been found...
+echo.
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :Menu1
 
 :usb1
+echo.
 echo The USB path [...] has been found...
+echo.
 echo Do you want to open it?
 echo.
 set /p where=
@@ -137,7 +162,9 @@ if not %where% == no goto :error
 goto :usb
 
 :usb2
+echo.
 echo The USB path [...] has been found...
+echo.
 echo Do you want to open it?
 echo.
 set /p where=
@@ -150,7 +177,9 @@ pause
 goto :usb
 
 :usb3
+echo.
 echo The USB path [...] has been found...
+echo.
 echo Do you want to open it?
 echo.
 set /p where=
@@ -162,51 +191,74 @@ if not %where% == no goto :error
 goto :usb
 
 :close
+echo.
 echo Closing the function...
+echo.
 pause
 goto :Menu1
 
 :google
+echo.
 echo Starting Google...
+echo.
 pause
+echo.
 start www.google.com
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :start
 
 :notepad
+echo.
 echo Starting Notepad...
+echo.
 pause
 start Notepad
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :start
 
 :WindowsXPAV2.0
+echo.
 echo Starting WindowsXPAV2.0...
+echo.
 pause
 start WindowsXPAV2.0.bat
 pause
+echo.
 echo Closing...
+echo.
 pause
 goto :start
 
 
 :error
+echo.
 echo The command typed is not available
+echo.
 pause
 goto :Menu1
 
 :error1
+echo.
 echo The program that you want to start is not available
+echo.
 pause
 goto :start
 
 :exit
+echo.
 echo Exiting...
+echo.
 pause
-echo thank you for using PDF_Editor
+echo.
+echo thank you for using Main Terminal
+echo.
 pause
 exit
