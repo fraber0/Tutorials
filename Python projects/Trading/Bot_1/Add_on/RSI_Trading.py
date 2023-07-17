b9d4a38cfe3833c40026dc3f193ef4670737e226
Elycor74 <133437175+Elycor74@users.noreply.github.com>
2023-07-17 21:19:35 +0200

    Add files via upload

---
+++ Python projects/Trading/Bot_1/Add_on/RSI_Trading.py
@@ -0,0 +1,23 @@
+def rsi_trading(trader, symbol, timeframe='1d', limit=100):
+    ohlcv = trader.exc.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
+    df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    
+    delta = df['close'].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(window=14).mean()
+    avg_loss = loss.rolling(window=14).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    
+    if rsi.iloc[-1] < 30:
+        print(f'RSI is below 30, opening long position')
+        position_size = 0.5
+        trader.buy(symbol, position_size)
+    elif rsi.iloc[-1] > 70:
+        print(f'RSI is above 70, opening short position')
+        position_size = 0.5
+        trader.sell(symbol, position_size)
+
+    except Exception as e:
+        print(f'Error while executing RSI trading strategy: {e}')
